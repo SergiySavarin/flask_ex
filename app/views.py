faf0413728fc1f56e3a1132c8b8337244ec8af1d
SergiySavarin <vir.host@gmail.com>
2016-09-08 20:36:27 +0300

    add ajax response

--- app/views.py
+++ app/views.py
@@ -3,12 +3,13 @@
 
 from flask import render_template
 from flask import request
+from flask import jsonify
 
 from app import app
 from app import db
 from forms import LoginForm
 from models import User
-
+import ipdb
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -19,6 +20,8 @@ def index():
                         password=request.form['password'])
         db.session.add(req_user)
         db.session.commit()
-        # ipdb.set_trace()
+    # ipdb.set_trace()
+    if request.is_xhr:
+        return jsonify({'message': 'Request received!'})
     return render_template('index.html', user=user,
                             title='Secret', form=form)
